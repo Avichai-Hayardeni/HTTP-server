8e23aee2e329f1cdf9142af2f7cf28eb77ece78d
Avichai-Hayardeni <avichai.hayardeni@gmail.com>
2022-01-16 14:36:34 +0200

    initialized and added a bunch of cool html responses & css

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+from flask import Flask, request, Response, render_template, send_file
+app = Flask(__name__)
 
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-
-        self.wfile.write("SUP".encode())
+@app.route("/style.css")
+def send_css():
+    return send_file("style.css")
 
 
-def main():
-    PORT = 1234
-    server = HTTPServer(('', PORT), Handler)
-    print("Server is up and running")
-    server.serve_forever()
-    server.close()
+@app.route("/")
+def welcome_page():
+    return render_template('welcome response.html', ip=request.remote_addr)
 
 
-if __name__ == "__main__":
-    main()
+@app.route("/sum")
+def sum():
+    if "num1" in request.args and "num2" in request.args:
+        num1 = int(request.args.get("num1"))
+        num2 = int(request.args.get("num2"))
+        return render_template('sum response.html', sum=num1+num2)
+    else:
+        return Response("idiot", status=418)
+
+
+@app.route("/myname")
+def myname():
+    if len(request.args) != 1 or "name" not in request.args:
+        return Response("idiot", status=400)
+    return render_template('myname response.html', name=request.args.get("name"))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
